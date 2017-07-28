4d98f816a243a73b6ecb5108dc90b3c4a95d9c80
Paul Hoskinson <plhosk@gmail.com>
2017-07-27 23:18:08 -0700

    Simple email obfuscation

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,21 +2,12 @@ import React from 'react'
 
 import './Contact.css'
 
-const abc = 'mai'
-const use = 'plhosk'
-const ser = '@gm'
-const tld = 'om'
-
 const Contact = () => (
   <div className="Contact-outer-div">
     <div className="Contact-email">
-      I can be reached by email at the following address: <a
-        className="Contact-email-link"
-        href={`${abc}lto:${use}${ser}ail.c${tld}`}
-        target="_blank"
-      >
-        {`${use}${ser}ail.c${tld}`}
-      </a>
+      I can be reached by email at the
+      following link: <a href="mailto:&#112;&#108;&#104;&#111;&#115;&#107;&#64;&#103;&#109;&#97;&#105;l.&#99;om" target="_blank" rel="noopener noreferrer">
+&#112;&#108;&#104;&#111;&#115;&#107;&#64;&#103;&#109;&#97;&#105;l.&#99;om</a>
     </div>
   </div>
 )
