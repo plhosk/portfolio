4cf840aff26c264ca03a042e30b757653c2f42b8
Paul Hoskinson <plhosk@gmail.com>
2017-07-27 23:57:20 -0700

    comment out babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-filename-extension */
+// import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
