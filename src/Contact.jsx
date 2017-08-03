f73f18b40353a669b18e33aac301331bc4728fe0
Paul Hoskinson <plhosk@gmail.com>
2017-08-02 19:14:49 -0700

    update footer

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => (
   <div className="Contact-outer-div">
     <div className="Contact-email">
       I can be reached by email at the
-      following link: <a href="mailto:&#112;&#108;&#104;&#111;&#115;&#107;&#64;&#103;&#109;&#97;&#105;l.&#99;om" target="_blank" rel="noopener noreferrer">
+      following address: <a href="mailto:&#112;&#108;&#104;&#111;&#115;&#107;&#64;&#103;&#109;&#97;&#105;l.&#99;om" target="_blank" rel="noopener noreferrer">
 &#112;&#108;&#104;&#111;&#115;&#107;&#64;&#103;&#109;&#97;&#105;l.&#99;om</a>
     </div>
   </div>
